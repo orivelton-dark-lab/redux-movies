f9abc71680e6e807a6c69d8dbd0838614bd8ba5d
Örivelton Cesar <oconceicao@recordtv.com.br>
2019-11-24 19:56:18 +0000

    chore: reducer, actions, components

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+const moviesReducer = () => {
+  return [
+    {title: 'Spider-Man', releaseDate: '05-07-2017', rating: 7.4},
+    {title: 'Spider-Man', releaseDate: '05-07-2017', rating: 7.4},
+    {title: 'Spider-Man', releaseDate: '05-07-2017', rating: 7.4},
+    {title: 'Spider-Man', releaseDate: '05-07-2017', rating: 7.4},
+    {title: 'Spider-Man', releaseDate: '05-07-2017', rating: 7.4},
+    {title: 'Spider-Man', releaseDate: '05-07-2017', rating: 7.4}
+  ]
+}
+
+const selectedMovieReducer = (state) => {
+
+}
