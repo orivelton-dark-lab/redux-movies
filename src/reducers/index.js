a7ec13c646fd4bdc8bc7aa629766190e5f104e6a
Örivelton Cesar <oconceicao@recordtv.com.br>
2019-11-24 21:10:33 +0000

    chore: Added reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { conbineReducers } from 'redux';
+
 const moviesReducer = () => {
   return [
     {title: 'Spider-Man', releaseDate: '05-07-2017', rating: 7.4},
@@ -9,6 +11,16 @@ const moviesReducer = () => {
   ]
 }
 
-const selectedMovieReducer = (state) => {
-
+const selectedMovieReducer = (state = null, action) => {
+  switch(action.type) {
+    case 'MOVIE_SELECTED':
+      return action.playload
+    default:
+      return state
+  }
 }
+
+export default combineReducers( {
+  movies: moviesReducer,
+  selectedMovie: selectedMovieReducer
+});
